d0fd2cbd9df22653c7d9e1bb6a23eb9efc6b37f6
Antonio Renque <renquegallego76@gmail.com>
2019-10-16 11:27:12 +0200

    AÑADO DELETE EDIFICIO

--- DELETE.sql
+++ DELETE.sql
@@ -1,5 +1,7 @@
-DELETE TABLE USUARIO
+DELETE  USUARIO
 DELETE  Curso
 where Nombre="Francisco";
-DELETE TABLE AULA
-where Nombre="1ªASIR"
+DELETE  AULA
+where Nombre="1ªASIR";
+DELETE EDIFICIO
+where NUMERO=2; 
